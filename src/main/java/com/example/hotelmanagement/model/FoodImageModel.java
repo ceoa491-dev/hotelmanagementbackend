0f11246740aba81c248c3af13094b4851933f4ab
Dinesh <ceoa491@gmail.com>
2026-04-08 18:50:45 +0530

    Error Solved

--- src/main/java/com/example/hotelmanagement/model/FoodImageModel.java
+++ src/main/java/com/example/hotelmanagement/model/FoodImageModel.java
@@ -11,6 +11,31 @@ public class FoodImageModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private Long foodId;
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Long getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(Long foodId) {
+        this.foodId = foodId;
+    }
+
     @Lob
     @Column(columnDefinition = "LONGBLOB")
     private byte[] image;
